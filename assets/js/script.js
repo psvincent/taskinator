02558f2e0557ed939593774e8132a065931e5275
psvincent12 <psvincent12@gmail.com>
2022-03-24 05:43:06 -0500

    Working input and select values are showing on HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,29 @@ var createTaskHandler = function(event) {
     // The event.preventDefault makes sure the browser doesn't reload the page everytime the function is called.
     event.preventDefault();
 
+    // This pulls the input with the name equal to "task-name" and then also takes only the value from it instead of the whole object and the value is what the user enters.
+    // The value of the variable taskNameInput will now be whatever the user enters in the input element.
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // This pulls the select value the user selects on the dropdown menu.
+    // The value of the variable taskTypeInput is now whatever the user enters on the dropdown select element.
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     // This variable creates a new li element.
     var listItemEl = document.createElement("li");
     // This .className assigns the listItemEl variable a class so it is styled correctly.
     listItemEl.className = "task-item";
-    // This .textContent is the text that is in the newly created li.
-    listItemEl.textContent = "This is a new task.";
+    // This taskInfoEl variable creates a div that will hold both the users input value and select value.
+    var taskInfoEl = document.createElement("div");
+    // This assigns the class to the div container above.
+    taskInfoEl.className = "task-info";
+    // This gets the actual content onto the different divs and styles them accordingly using classes and uses the variables to enter the correct values where they need to be.
+    // This also uses .innerHTML which allows you to write html with the tags and everything in it where as .textContent only allows you to enter text without html tags.
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // This .appendChild takes the listItemEl which is the new li and puts the div with the user given values inside of it.
+    listItemEl.appendChild(taskInfoEl);
     // this .appendChild takes the tasksToDoEl variable that is assigned to the ul and puts the newly created li that was made above in it.
     tasksToDoEl.appendChild(listItemEl);
+    // The li is bigger than the div so the listItemElvariable is bigger than the taskInfoEl variable.
 };
 
 // The on submit listens for either a button to be clicked with a type attribute value of submit or when a user presses enter on their keyboard.
